6435700adbfa45a71c59b8d10e86458285244f38
Dragonsangel <lindebernd@gmail.com>
2025-10-22 12:33:49 +0200

    Main branch change

--- GitMergeConflictDemo/Program.cs
+++ GitMergeConflictDemo/Program.cs
@@ -1,2 +1,2 @@
 ﻿Console.WriteLine("This is a simple dome of what to do when a merge conflict happens.");
-Console.WriteLine("I am now in the Main branch");
+Console.WriteLine("I am now in the Main branch. (I forgot the point)");
